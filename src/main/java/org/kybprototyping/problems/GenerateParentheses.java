39f23505fc7f032a509bee79eba651fb65c7400b
Onur Kayabasi <onurbpm@outlook.com>
2022-12-17 21:08:14 +0100

    Continue to GenerateParentheses

--- src/main/java/org/kybprototyping/problems/GenerateParentheses.java
+++ src/main/java/org/kybprototyping/problems/GenerateParentheses.java
@@ -3,10 +3,8 @@ package org.kybprototyping.problems;
 import org.kybprototyping.BootstrapHelper;
 import org.kybprototyping.ConsoleUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import lombok.Data;
 
@@ -50,6 +48,8 @@ public class GenerateParentheses implements AlgorithmProblem<GenerateParentheses
 		// 101100
 		// 101010
 
+		// ["(())(())", "(())()()", "()()(())", "()()()()"]
+
 		// By the solution, let's test n = 4, it could shift n-1 times
 		// 11110000
 		// 11101000
@@ -58,6 +58,13 @@ public class GenerateParentheses implements AlgorithmProblem<GenerateParentheses
 		// 11011000
 		// 11010100
 		// 11010010
+
+		// missing ones
+		// 10101010
+		// 10101100
+		// 11001010
+		// 11001100
+
 		// 10111000
 		// 10110100
 		// 10110010
@@ -65,19 +72,19 @@ public class GenerateParentheses implements AlgorithmProblem<GenerateParentheses
 		// - Shift the latest 1 to right side n-1 times
 		// - Shift the left 1 of the latest 1 to right side n-1 times ...
 
-
-		// 1 -> 1			0 +1
-		// 3 -> 5			2 - 2 +1
-		// 4 -> 10 		3 - 3 - 3 +1
-		// 5 -> ?			4 - 4 - 4 - 4 +1
+		// 1 -> 1 0 +1
+		// 3 -> 5 2 - 2 +1
+		// 4 -> 10 3 - 3 - 3 +1
+		// 5 -> ? 4 - 4 - 4 - 4 +1
 		// ((n-1) * (n-1)) + 1
-		String[] result = new String[((n-1) * (n-1)) + 1];
+
+		String[] result = new String[((n - 1) * (n - 1)) + 1];
 
 		// start
-		char[] initial = new char[n*2];
+		char[] initial = new char[n * 2];
 		for (int i = 0; i <= 1; i++) {
 			for (int j = n * i; j < n * (i + 1); j++) {
-				if(i == 0) {
+				if (i == 0) {
 					initial[j] = '(';
 				} else {
 					initial[j] = ')';
@@ -87,10 +94,10 @@ public class GenerateParentheses implements AlgorithmProblem<GenerateParentheses
 		result[0] = new String(initial);
 
 		int resultIx = 1;
-		char[] tmp = new char[n*2];
-		for (int i = n-1; i > 0; i--) {
-			for (int j = i+1; j < (n*2)-1; j++) {
-				tmp = initial;
+		char[] tmp = null;
+		for (int i = n - 1; i > 0; i--) {
+			for (int j = n; j < (n * 2) - 1; j++) {
+				tmp = initial.clone();
 				tmp[i] = ')';
 				tmp[j] = '(';
 				result[resultIx] = new String(tmp);
