929cfed109826524161799bb30281918ab9c53f4
Onur Kayabasi <onurbpm@outlook.com>
2023-11-03 19:28:41 +0100

    Continue to SubstringWithConcatenationOfAllWords

--- src/main/java/org/kybprototyping/problems/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/org/kybprototyping/problems/SubstringWithConcatenationOfAllWords.java
@@ -3,7 +3,6 @@ package org.kybprototyping.problems;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 final class SubstringWithConcatenationOfAllWords {
 
@@ -11,33 +10,31 @@ final class SubstringWithConcatenationOfAllWords {
 
 	@SuppressWarnings({"java:S135", "java:S3776"})
 	static List<Integer> findSubstring(String s, String[] words) {
-		Set<Integer> result = checkIfAllSame(s, words);
-
-		if (result.size() == s.length() / words.length) {
-			return new ArrayList<>(result);
-		}
+		HashSet<Integer> result = new HashSet<>();
 
 		int wordLength = words[0].length();
-
+		HashSet<String> checkedWords = new HashSet<>(words.length);
 		int i = 0;
-		int fromIndex = 0;
-		HashSet<String> temp = new HashSet<>(wordLength);
+		int fromIx = 0;
 		while (i < words.length) {
-			int startingIx = s.indexOf(words[i], fromIndex);
+			if (checkedWords.contains(words[i])) {
+				i = i + 1;
+				fromIx = 0;
+				continue;
+			}
+
+			int startingIx = s.indexOf(words[i], fromIx);
 
 			if (startingIx == -1 || result.contains(startingIx)) {
+				checkedWords.add(words[i]);
 				i = i + 1;
-				fromIndex = 0;
-			} else if (temp.contains(words[i] + startingIx)) {
-				i = i + 1;
-				fromIndex = 0;
+				fromIx = 0;
 			} else {
 				HashSet<Integer> usedIndices = new HashSet<>(words.length);
 				usedIndices.add(i);
-				temp.add(words[i] + startingIx);
 
-				fromIndex = startingIx + wordLength;
-				for (int j = fromIndex; j + wordLength <= s.length(); j += wordLength) {
+				fromIx = startingIx + wordLength;
+				for (int j = fromIx; j + wordLength <= s.length(); j += wordLength) {
 					if (usedIndices.size() == words.length) {
 						break;
 					}
@@ -48,32 +45,19 @@ final class SubstringWithConcatenationOfAllWords {
 					if (existentWordIx != -1) {
 						usedIndices.add(existentWordIx);
 					} else {
-						fromIndex = startingIx + 1;
+						fromIx = startingIx + 1;
 						break;
 					}
 				}
 
 				if (usedIndices.size() == words.length) {
 					result.add(startingIx);
-					fromIndex = startingIx + 1;
+					fromIx = startingIx + 1;
 				}
 			}
 		}
 
 		return new ArrayList<>(result);
-	}
-
-	private static Set<Integer> checkIfAllSame(String s, String[] words) {
-		HashSet<Integer> result = new HashSet<>();
-
-		int i = 0;
-		String joined = String.join("", words);
-		while (s.indexOf(joined, i) != -1) {
-			result.add(i);
-			i = i + joined.length();
-		}
-
-		return result;
 	}
 
 	private static int indexOf(String[] arr, String str, HashSet<Integer> exclusions) {
