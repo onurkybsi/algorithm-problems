be48bd0b1726871d5f1ff288968f7c517b89f080
Onur Kayabasi <onurbpm@outlook.com>
2023-11-07 21:59:02 +0100

    Continue to NextPermutation

--- src/main/java/org/kybprototyping/problems/NextPermutation.java
+++ src/main/java/org/kybprototyping/problems/NextPermutation.java
@@ -5,25 +5,62 @@ final class NextPermutation {
   private NextPermutation() {}
 
   public static void nextPermutation(int[] nums) {
+    int i = nums.length - 1, j = i - 1;
 
-    // 2 9 0 8
-    // 2 9 8 0
-    // 8 0 2 9
-    // 8 0 9 2
-    // 8 2 0 9
-    // 8 2 9 0
-    // 8 9 2 0
-    // 9 0 2 8
-
-    int j = nums.length - 1;
-    for (int i = nums.length - 1; i >= 0; i--) {
+    while (i > 0) {
       if (nums[i] > nums[j]) {
         int temp = nums[j];
         nums[j] = nums[i];
         nums[i] = temp;
+
+        countingSort(nums, j + 1, nums.length);
+        return;
+      } else if (j == 0) {
+        i -= 1;
+        j = i - 1;
+      } else {
+        j -= 1;
       }
     }
 
+    countingSort(nums, 0, nums.length);
+  }
+
+  public static void countingSort(int[] arr, int from, int to) {
+    int max = findMax(arr, from, to); // O(n)
+
+    // initialized with zero values.
+    int[] countingArr = new int[max + 1]; // O(max + 1) in RAM model
+    for (int i = from; i < to; i++) { // O(n)
+      countingArr[arr[i]] += 1;
+    }
+
+    for (int i = 1; i < max + 1; i++) { // O(max + 1)
+      countingArr[i] += countingArr[i - 1];
+    }
+
+    int[] temp = new int[to - from];
+    // iterating from the end at this point makes the algorithm stable.
+    for (int i = to - 1; i >= from; i--) { // O(n)
+      temp[countingArr[arr[i]] - 1] = arr[i];
+      countingArr[arr[i]]--;
+    }
+
+    for (int i = 0; i < temp.length; i++) { // O(n)
+      arr[from + i] = temp[i];
+    }
+  }
+
+  private static int findMax(int[] arr, int from, int to) {
+    int max = arr[from];
+
+    for (int i = from; i < to; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+
+    return max;
   }
 
 }
--- src/test/java/org/kybprototyping/problems/NextPermutationTest.java
+++ src/test/java/org/kybprototyping/problems/NextPermutationTest.java
@@ -1,5 +1,6 @@
 package org.kybprototyping.problems;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,12 +10,24 @@ class NextPermutationTest {
 
   @ParameterizedTest
   @MethodSource("provideTestInputsAndActualOutputs")
-  void nextPermutation_Should_Set_Given_Nums_As_Its_Next_Lexicographically_Greater_Permutation() {
+  void nextPermutation_Should_Set_Given_Nums_As_Its_Next_Lexicographically_Greater_Permutation(
+      int[] nums, int[] expectedResult) {
+    // given
 
+    // when
+    NextPermutation.nextPermutation(nums);
+
+    // then
+    assertArrayEquals(expectedResult, nums);
   }
 
   private static Stream<Arguments> provideTestInputsAndActualOutputs() {
-    return Stream.of(Arguments.of());
+    return Stream.of(Arguments.of(new int[] {2, 9, 0, 8}, new int[] {2, 9, 8, 0}),
+        Arguments.of(new int[] {1, 2, 3}, new int[] {1, 3, 2}),
+        Arguments.of(new int[] {3, 2, 1}, new int[] {1, 2, 3}),
+        Arguments.of(new int[] {1, 1, 5}, new int[] {1, 5, 1}),
+        Arguments.of(new int[] {1, 3, 2}, new int[] {2, 1, 3}),
+        Arguments.of(new int[] {4, 2, 0, 2, 3, 2, 0}, new int[] {4, 2, 0, 3, 0, 2, 2}));
   }
 
 }
