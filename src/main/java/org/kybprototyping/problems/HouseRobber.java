d78ee1ae88f58fbceb8fe2ed2f2dc8a86292759f
Onur Kayabasi <o.kayabasi@outlook.com>
2024-11-24 20:58:43 +0300

    HouseRobber solved

--- src/main/java/org/kybprototyping/problems/HouseRobber.java
+++ src/main/java/org/kybprototyping/problems/HouseRobber.java
@@ -5,13 +5,13 @@ final class HouseRobber {
   private HouseRobber() {}
 
   public static int rob(int[] nums) {
-    int[] robbed = new int[nums.length + 1];
-    robbed[nums.length] = 0;
-    robbed[nums.length - 1] = nums[nums.length - 1];
+    int[] temp = new int[nums.length + 1];
+    temp[nums.length] = 0;
+    temp[nums.length - 1] = nums[nums.length - 1];
     for (int i = nums.length - 2; i >= 0; i--) {
-      robbed[i] = Math.max(robbed[i + 1], robbed[i + 2] + nums[i]);
+      temp[i] = Math.max(temp[i + 1], temp[i + 2] + nums[i]);
     }
-    return robbed[0];
+    return temp[0];
   }
 
 }
