c599350faa037ff0890def4d37125a22151af659
Onur Kayabasi <o.kayabasi@outlook.com>
2025-01-03 08:19:51 +0100

    BestTimeToBuyAndSellStock solved again

--- src/main/java/org/kybprototyping/problems/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/kybprototyping/problems/BestTimeToBuyAndSellStock.java
@@ -1,8 +1,12 @@
 package org.kybprototyping.problems;
 
+import java.util.Arrays;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
 final class BestTimeToBuyAndSellStock {
 
-  public static int maxProfit(int[] prices) {
+  public int maxProfit(int[] prices) {
     int minPrice = prices[0];
     int maxProfit = 0;
     for (int i = 0; i < prices.length; i++) {
@@ -14,6 +18,41 @@ final class BestTimeToBuyAndSellStock {
     }
 
     return maxProfit;
+  }
+
+  @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+  static class TopToBottom {
+
+    private int[] prices;
+    private int[][] memo;
+
+    public int maxProfit(int[] prices) {
+      this.prices = prices;
+      this.memo = new int[prices.length][2];
+      for (int[] m : this.memo)
+        Arrays.fill(m, -1);
+      return maxProfit(0, 1);
+    }
+
+    private int maxProfit(int ix, int isBuying) {
+      if (ix == prices.length) {
+        return 0;
+      }
+      if (this.memo[ix][isBuying] != -1) {
+        return this.memo[ix][isBuying];
+      }
+
+      int maxProfit = 0;
+      if (isBuying == 1) {
+        maxProfit = Math.max(-prices[ix] + maxProfit(ix + 1, 0), maxProfit(ix + 1, isBuying));
+      } else {
+        maxProfit = Math.max(prices[ix], maxProfit(ix + 1, isBuying));
+      }
+
+      this.memo[ix][isBuying] = maxProfit;
+      return maxProfit;
+    }
+
   }
 
 }
