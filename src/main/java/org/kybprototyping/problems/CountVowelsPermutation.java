66af47e8234edf7158fb1f515cd7825953116569
Onur Kayabasi <o.kayabasi@outlook.com>
2025-01-15 07:43:44 +0100

    CountVowelsPermutation solved

---
+++ src/main/java/org/kybprototyping/problems/CountVowelsPermutation.java
@@ -0,0 +1,52 @@
+package org.kybprototyping.problems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+final class CountVowelsPermutation {
+
+  private static final List<Character> chars = Arrays.asList('a', 'e', 'i', 'o', 'u');
+  private static final Map<Character, List<Character>> followingChars =
+      Map.of('a', Arrays.asList('e'), 'e', Arrays.asList('a', 'i'), 'i',
+          Arrays.asList('a', 'e', 'o', 'u'), 'o', Arrays.asList('i', 'u'), 'u', Arrays.asList('a'));
+
+  private HashMap<Pair, Integer> memo;
+
+  public int countVowelPermutation(int n) {
+    this.memo = new HashMap<>();
+
+    int count = 0;
+    for (Character c : chars)
+      count = (count + countVowelPermutation(c, n - 1)) % 1_000_000_007;
+    return count % 1_000_000_007;
+  }
+
+  private int countVowelPermutation(Character previousChar, int length) {
+    if (length == 0) {
+      return 1;
+    }
+
+    var pair = new Pair(previousChar, length);
+    if (this.memo.containsKey(pair)) {
+      return this.memo.get(pair);
+    }
+
+    int count = 0;
+    var followings = followingChars.get(previousChar);
+    for (Character following : followings) {
+      count = (count + countVowelPermutation(following, length - 1)) % 1_000_000_007;
+    }
+
+    this.memo.put(pair, count);
+    return count;
+  }
+
+  private record Pair(Character first, int second) {
+  }
+
+}
