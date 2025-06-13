d5d876a69205c3bbda13414e32e4d67fdf761874
Onur Kayabasi <o.kayabasi@outlook.com>
2025-06-13 18:05:10 +0200

    KthSmallestPrimeFraction solved

---
+++ src/main/java/org/kybprototyping/problems/KthSmallestPrimeFraction.java
@@ -0,0 +1,32 @@
+package org.kybprototyping.problems;
+
+import java.util.PriorityQueue;
+
+final class KthSmallestPrimeFraction {
+
+  private KthSmallestPrimeFraction() {}
+
+  public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+    PriorityQueue<Triple> queue =
+        new PriorityQueue<>((p1, p2) -> Double.compare(p1.first, p2.first));
+    for (int i = 0; i < arr.length; i++) {
+      int dividend = arr[i];
+
+      for (int j = 0; j < arr.length; j++) {
+        queue.add(new Triple((double) dividend / (double) arr[j], dividend, arr[j]));
+      }
+    }
+
+    while (k > 1) {
+      queue.remove();
+      k--;
+    }
+    var kthSmallestPrimeFraction = queue.remove();
+
+    return new int[] {kthSmallestPrimeFraction.second, kthSmallestPrimeFraction.third};
+  }
+
+  record Triple(Double first, int second, int third) {
+  }
+
+}
