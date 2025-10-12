993e9ea3f53e8f68a2f9f6dce4ce1504d1e6eac9
Onur Kayabasi <o.kayabasi@outlook.com>
2025-10-12 15:04:23 +0200

    Valid Parentheses solved

--- src/main/java/org/kybprototyping/problems/ValidParentheses.java
+++ src/main/java/org/kybprototyping/problems/ValidParentheses.java
@@ -1,66 +1,27 @@
 package org.kybprototyping.problems;
 
-import org.kybprototyping.BootstrapHelper;
-import org.kybprototyping.ConsoleUtils;
+import java.util.Map;
+import java.util.Stack;
 
-import java.util.List;
+final class ValidParentheses {
 
-import lombok.Data;
+	private ValidParentheses() {}
 
-public class ValidParentheses implements AlgorithmProblem<ValidParentheses.ValidParenthesesArgs> {
-	private static List<ValidParenthesesArgs> args = BootstrapHelper
-			.getProblemArgs(ValidParenthesesArgs.class);
+	private static final Map<Character, Character> brackets = Map.of('(', ')', '{', '}', '[', ']');
 
-	@Data
-	public static class ValidParenthesesArgs {
-		private String s;
-	}
-
-	@Override
-	public String getName() {
-		return "ValidParentheses";
-	}
-
-	@Override
-	public String getDescriptionLink() {
-		return "https://leetcode.com/problems/valid-parentheses/";
-	}
-
-	@Override
-	public List<ValidParenthesesArgs> getProblemArgs() {
-		return args;
-	}
-
-	@Override
-	public void runSolution(int argsOrder) {
-		// TODO Auto-generated method stub
-		boolean result = isValid(args.get(argsOrder - 1).getS());
-		ConsoleUtils.INSTANCE.info(result);
-	}
-
-	private static boolean isValid(String s) {
-		return false;
-	}
-
-	static char getPair(char onePair) {
-		if (onePair == '(') {
-			return ')';
+	public boolean isValid(String s) {
+		Stack<Character> openBrackets = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			boolean isOpenBracket = brackets.containsKey(s.charAt(i));
+			if (isOpenBracket) {
+				openBrackets.add(s.charAt(i));
+			} else if (!openBrackets.isEmpty() && brackets.get(openBrackets.peek()).equals(s.charAt(i))) {
+				openBrackets.pop();
+			} else {
+				return false;
+			}
 		}
-		if (onePair == ')') {
-			return '(';
-		}
-		if (onePair == '{') {
-			return '}';
-		}
-		if (onePair == '}') {
-			return '{';
-		}
-		if (onePair == '[') {
-			return ']';
-		}
-		if (onePair == ']') {
-			return '[';
-		}
-		return 0;
+		return openBrackets.isEmpty();
 	}
+
 }
